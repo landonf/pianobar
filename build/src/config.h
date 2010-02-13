f6f1d0f2e8d341e99db2cfc9fbd40014843c1d92
Steve Streza <stevestreza@gmail.com>
2010-02-13 15:38:15 -0500

    Added xcodeproj

---
+++ build/src/config.h
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+/* package name */
+#define PACKAGE "pianobar"
+
+/* used libraries */
+#define ENABLE_FAAD
+/* #undef ENABLE_MAD */
+
+#endif /* _CONFIG_H */
